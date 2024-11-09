ff649684136e788210161d2523affba6c766a3d5
Kocayamak <mikailkocayamak@gmail.com>
2024-11-09 16:55:27 +0300

    starting authform components

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,9 +1,144 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 type FormType = "sign-in" | "sign-up";
 
+const authFormSchema = (formType: FormType) => {
+  return z.object({
+    email: z.string().email(),
+    fullName:
+      formType === "sign-up"
+        ? z.string().min(2).max(50)
+        : z.string().optional(),
+  });
+};
+
 const AuthForm = ({ type }: { type: FormType }) => {
-  return <div></div>;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const formSchema = authFormSchema(type);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullName: "",
+      email: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+        <h1 className="form-title">
+          {type === "sign-in" ? "Giriş Yap" : "Kayıt Ol"}
+        </h1>
+        {type === "sign-up" && (
+          <FormField
+            control={form.control}
+            name="fullName"
+            render={({ field }) => (
+              <FormItem>
+                <div className="shad-form-item">
+                  <FormLabel className="shad-form-label">
+                    Kullanıcı Adı
+                  </FormLabel>
+
+                  <FormControl>
+                    <Input
+                      placeholder="Kullanıcı adınızı giriniz.."
+                      className="shad-input"
+                      {...field}
+                    />
+                  </FormControl>
+                </div>
+                <FormMessage className="shad-form-message" />
+              </FormItem>
+            )}
+          />
+        )}
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <div className="shad-form-item">
+                <FormLabel className="shad-form-label">E-Posta</FormLabel>
+
+                <FormControl>
+                  <Input
+                    placeholder="E-Posta adresinizi giriniz..."
+                    className="shad-input"
+                    {...field}
+                  />
+                </FormControl>
+              </div>
+              <FormMessage className="shad-form-message" />
+            </FormItem>
+          )}
+        />
+
+        <Button
+          type="submit"
+          className="form-submit-button"
+          disabled={isLoading}
+        >
+          {type === "sign-in" ? "Giriş Yap" : "Kayıt Ol"}
+
+          {isLoading && (
+            <Image
+              src="/assets/icons/loader.svg"
+              alt="loader"
+              width={24}
+              height={24}
+              className="ml-2 animate-spin"
+            />
+          )}
+        </Button>
+
+        {errorMessage && <p className="error-message">*{errorMessage}</p>}
+
+        <div className="body-2 flex justify-center">
+          <p className="text-light-100">
+            {type === "sign-in"
+              ? "Henüz hesbaınız yok mu ?"
+              : "Hesabınız var mı ?"}
+          </p>
+
+          <Link
+            href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+            className="ml-1 font-medium text-brand"
+          >
+            {""}
+            {type === "sign-in" ? "Kayıt Ol" : "Giriş Yap"}
+          </Link>
+        </div>
+      </form>
+    </Form>
+  );
 };
 
 export default AuthForm;
