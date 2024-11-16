33cc095adbcb229b66b207cf879696b80efa7f30
Kocayamak <mikailkocayamak@gmail.com>
2024-11-16 22:37:33 +0300

    file uploader componenti tamamlandı ve thumnail eklendi ve fake progress eklendi.

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -3,8 +3,9 @@
 import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { Button } from "@/components/ui/button";
-import { cn, getFileType } from "@/lib/utils";
+import { cn, convertFileToUrl, getFileType } from "@/lib/utils";
 import Image from "next/image";
+import Thumbnail from "@/components/Thumbnail";
 
 interface Props {
   ownerId: string;
@@ -17,6 +18,15 @@ const FileUploader = ({ ownerId, accountıd, className }: Props) => {
     setFiles(acceptedFiles);
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  const handleRemoveFile = (
+    e: React.MouseEvent<HTMLImageElement, MouseEvent>,
+    fileName: string,
+  ) => {
+    e.stopPropagation();
+
+    setFiles((prevFiles) => prevFiles.filter((file) => file.name !== fileName));
+  };
 
   return (
     <div {...getRootProps()} className="cursor-pointer">
@@ -41,7 +51,31 @@ const FileUploader = ({ ownerId, accountıd, className }: Props) => {
                 key={`${file.name}-${index}`}
                 className="uploader-preview-item"
               >
-                <div className="flex items-center gap-3"></div>
+                <div className="flex items-center gap-3">
+                  <Thumbnail
+                    type={type}
+                    extension={extension}
+                    url={convertFileToUrl(file)}
+                  />
+
+                  <div className="preview-item-name">
+                    {file.name}
+                    <Image
+                      src="/assets/icons/file-loader.gif"
+                      alt="file-loader"
+                      width={80}
+                      height={26}
+                    />
+                  </div>
+
+                  <Image
+                    src="/assets/icons/remove.svg"
+                    alt="remove"
+                    width={24}
+                    height={24}
+                    onClick={(e) => handleRemoveFile(e, file.name)}
+                  />
+                </div>
               </li>
             );
           })}
