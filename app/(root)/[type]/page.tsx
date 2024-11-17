17cab928ffe3d9e248b16d2471c7626a31284cfa
Kocayamak <mikailkocayamak@gmail.com>
2024-11-17 17:40:07 +0300

    filecard component added.

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -3,6 +3,7 @@ import Sort from "@/components/Sort";
 import { getFiles } from "@/lib/actions/file.actions";
 import { getTypeName } from "@/lib/utils";
 import { Models } from "node-appwrite";
+import Card from "@/components/Card";
 
 const Page = async ({ params }: SearchParamProps) => {
   const type = ((await params)?.type as string) || "";
@@ -29,9 +30,7 @@ const Page = async ({ params }: SearchParamProps) => {
       {files.total > 0 ? (
         <section className="file-list">
           {files.documents.map((file: Models.Document) => (
-            <h1 className="h1" key={file.$id}>
-              {file.name}
-            </h1>
+            <Card key={file.$id} file={file} />
           ))}
         </section>
       ) : (
