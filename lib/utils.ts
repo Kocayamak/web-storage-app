73f7e07280547190d8e2f1290a4f6c6555189fc3
Kocayamak <mikailkocayamak@gmail.com>
2024-11-19 21:53:44 +0300

    formatted date düzenlendi.

--- lib/utils.ts
+++ lib/utils.ts
@@ -82,34 +82,29 @@ export const formatDateTime = (isoString: string | null | undefined) => {
 
   const date = new Date(isoString);
 
-  // Get hours and adjust for 12-hour format
-  let hours = date.getHours();
-  const minutes = date.getMinutes();
-  const period = hours >= 12 ? "pm" : "am";
-
-  // Convert hours to 12-hour format
-  hours = hours % 12 || 12;
-
-  // Format the time and date parts
-  const time = `${hours}:${minutes.toString().padStart(2, "0")}${period}`;
-  const day = date.getDate();
-  const monthNames = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
+  const months = [
+    "Ocak",
+    "Şubat",
+    "Mart",
+    "Nisan",
+    "Mayıs",
+    "Haziran",
+    "Temmuz",
+    "Ağustos",
+    "Eylül",
+    "Ekim",
+    "Kasım",
+    "Aralık",
   ];
-  const month = monthNames[date.getMonth()];
 
-  return `${time}, ${day} ${month}`;
+  const [day, month, year, time] = [
+    date.getDate(),
+    months[date.getMonth()],
+    date.getFullYear(),
+    `${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}`,
+  ];
+
+  return `${time}, ${day} ${month} ${year}`;
 };
 
 export const getFileIcon = (
